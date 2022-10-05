f5ac2cd8eeaca34f4709ffb6c641f3b1f712f2c0
zwj4031 <dnyl@qq.com>
2022-10-05 20:28:14 +0800

    fix bug

--- uefi.sh
+++ uefi.sh
@@ -4,9 +4,9 @@
  cp config/general-efi.h config/general.h 
  cp config/branding-efi.h config/branding.h 
  make clean
-make -j8 bin-x86_64-efi/ipxe.efi
-make -j8 bin-x86_64-efi/snponly.efi
-make -j8 bin-x86_64-efi/ipxe.efidrv
-make -j8 bin-x86_64-linux/tap.linux
-make -j8 bin-x86_64-linux/tests.linux
-make -j8 bin-x86_64-efi/ipxe.efirom  
+make -j 4 -C src bin-x86_64-efi/ipxe.efi
+make -j 4 -C src bin-x86_64-efi/snponly.efi
+make -j 4 -C src bin-x86_64-efi/ipxe.efidrv
+make -j 4 -C src bin-x86_64-linux/tap.linux
+make -j 4 -C src bin-x86_64-linux/tests.linux
+make -j 4 -C src bin-x86_64-efi/ipxe.efirom  
